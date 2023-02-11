268c3460b76855719641e7d747b9aad0e5239eff
sleeplesseditor <josh@bryson.me.uk>
2023-02-11 19:01:52 +0000

    Vertex and Fragment shaders added

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import vertexShader from './shaders/vertex.glsl';
+import fragmentShader from './shaders/fragment.glsl';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -8,17 +10,25 @@ const camera = new THREE.PerspectiveCamera(
     1000
 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
 renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), new THREE.MeshBasicMaterial({
-    color: 0xFF0000
-}));
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(5, 50, 50), 
+    new THREE.ShaderMaterial({
+        vertexShader,
+        fragmentShader
+        // map: new THREE.TextureLoader().load('./img/globe.jpg')
+    })
+);
 
 scene.add(sphere);
 
-camera.position.z = 10;
+camera.position.z = 15;
 
 function animate() {
     requestAnimationFrame(animate)
