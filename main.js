1b8d0e6a3ded7933e659347375c31e654535ca8c
sleeplesseditor <josh@bryson.me.uk>
2023-02-12 18:22:24 +0000

    Stars added to background

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
 import vertexShader from './shaders/vertex.glsl';
 import fragmentShader from './shaders/fragment.glsl';
 import atmosphereVertexShader from './shaders/atmosphereVertex.glsl';
@@ -46,15 +47,51 @@ const atmosphere = new THREE.Mesh(
 
 atmosphere.scale.set(1.1, 1.1, 1.1)
 
-scene.add(sphere);
-scene.add(atmosphere)
+scene.add(atmosphere);
+
+const group = new THREE.Group();
+group.add(sphere);
+scene.add(group);
+
+const starGeometry = new THREE.BufferGeometry();
+const starMaterial = new THREE.PointsMaterial({
+    color: 0xffffff
+});
+
+const starVertices = []
+for (let i = 0; i < 10000; i++) {
+    const x = (Math.random() - 0.5) * 2000;
+    const y = (Math.random() - 0.5) * 2000;
+    const z = -Math.random() * 2000;
+    starVertices.push(x, y, z)
+};
+
+starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3))
+
+const stars = new THREE.Points(starGeometry, starMaterial);
+scene.add(stars);
 
 camera.position.z = 15;
+
+const mouse = {
+    x: undefined,
+    y: undefined
+};
 
 function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera);
-    sphere.rotation.y += 0.01;
+    sphere.rotation.y += 0.002;
+    gsap.to(group.rotation, {
+        x: -mouse.y * 0.3, 
+        y: mouse.x * 0.5,
+        duration: 2
+    })
 }
 
 animate();
+
+addEventListener('mousemove', () => {
+    mouse.x = (event.clientX / innerWidth) * 2 - 1;
+    mouse.y = (event.clientY / innerHeight) * 2 + 1;
+});
